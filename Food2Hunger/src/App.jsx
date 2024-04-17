acb98a7c3a7c56cc79d739b12a1b8383cbd5723a
bishalbhattarai25 <bishalbhattrai117@gmail.com>
2024-04-17 11:58:39 +0545

    routing setup with navbar included

--- Food2Hunger/src/App.jsx
+++ Food2Hunger/src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import Navbar from './Components/Navigation/Navbar'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Blog from './Pages/Blog'
+import Marketplace from './Pages/Marketplace'
+import ContactUs from './Pages/ContactUs'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <p className='text-red-900'>bishal</p>
+     <Router>
+     <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/Blog' element={<Blog/>}></Route>
+        <Route path='/Marketplace' element={<Marketplace/>}></Route>
+        <Route path='/Contactus' element={<ContactUs/>}></Route>
+      </Routes>
+     </Router>
     </>
   )
 }
