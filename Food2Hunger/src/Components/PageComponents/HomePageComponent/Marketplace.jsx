2b9c3a14a0ec03aaa5a23f45f28f43f632dc91f9
bishalbhattarai25 <bishalbhattrai117@gmail.com>
2024-04-17 19:27:20 +0545

    connection of frontend and backend is in progress

--- Food2Hunger/src/Components/PageComponents/HomePageComponent/Marketplace.jsx
+++ Food2Hunger/src/Components/PageComponents/HomePageComponent/Marketplace.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import { MarketplaceData } from './MarketplaceData';
 import axios from 'axios';
 
-
 function Marketplace() {
-
+  const [data, setData]=useState([])
+  const getData=()=>{
+    try {
+      axios.get('http://localhost:3000/Market').then(res=>{
+        console.log(res);
+        setData([...res.data.data])
+      })
+    } catch (error) {
+      console.log(error);
+    }
+    }
+    useEffect(() => {
+      getData()
+    }, []);
   return (
     
     <div>
@@ -13,14 +25,15 @@ function Marketplace() {
 
    <div className='flex gap-10 px-10 '>
      {
-         MarketplaceData.map((val,i)=>{
+         data.map((val,i)=>{
+          let image = `http://localhost:3000/public/${val.image}`
              return(
                  <div className='flex flex-col items-start gap-3 bg-white rounded-md p-5 drop-shadow-xl'>
-                 <img src={val.img} alt="bg" className=' h-56 w-full rounded-md' />
+                 <img src={image} alt="bg" className=' h-56 w-full rounded-md' />
                  <div className='font-bold text-xl'>{val.title}</div>
                  <div className='text-slate-500'>{val.description}</div>
                  <div className='font-bold '>{val.price}</div>
-                 <div className='bg-[#F74B1B] text-white px-7 py-1 rounded-full'>{val.btn}</div>
+                 <div className='bg-[#F74B1B] text-white px-7 py-1 rounded-full'>See More</div>
                  </div>
              );
          })
