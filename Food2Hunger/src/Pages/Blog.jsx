e8e2874220607a598c20ccf66d33280dbaa6725d
ayush <ayushkhatri.105@gamil.com>
2024-04-18 06:35:52 +0545

    improving CSS of marketplace

--- Food2Hunger/src/Pages/Blog.jsx
+++ Food2Hunger/src/Pages/Blog.jsx
@@ -3,15 +3,15 @@ import React from "react";
 function Blog() {
   return (
     <>
-      <div className="mainscreen">
-        <div className=" w-full h-full flex-row " id="firstsection">
-          <div className="leftpartofscreen">
+      <div className="mainscreen  ">
+        <div className=" w-full h-full  flex-row   " id="firstsection">
+          <div className="leftpartofscreen ">
             <div
               id="first line of text"
-              className="text-[32px] font-semibold absolute top-28 left-16 gap-4 flex-col tracking-wider"
+              className="text-[32px] font-semibold absolute top-28 left-16 gap-4 flex-col tracking-wider "
             >
-              Welcome to
-              <div className="text-orange-600 font-extrabold uppercase">
+              <h1 className="font-bold text-5xl text-blue-500 pb-4 pt-16">Welcome to</h1>
+              <div className="text-black font-extrabold uppercase">
                 Blog page of food2hunger
               </div>
             </div>
@@ -19,30 +19,24 @@ function Blog() {
               id="second 3 lines of text"
               className="mt-12 absolute top-48 left-16 text-xl tracking-wide flex flex-col gap-1 "
             >
-              Here, we present you amazing post and various achievements of
-              NGOs.
-              <div className="flex-col mt-6 ">
-                Enjoy the beautiful memories
-                <div className="text-orange-500 font-sans font-bold">Gui</div>
-              </div>
-              Click the button below to view
+              <p className="text-3xl font-semibold capitalize pt-24">Here, we present <span className="text-green-600 font-bold  ">amazing post</span><br/>
+              <div className="flex py-10 gap-1"> and various <div className="text-orange-600 font-bold">achievements</div> of
+              NGOs.</div>
+              
+              </p>
+             
+               
             </div>
             <div
               id="for buttons"
               className="absolute top-[400px] left-16 flex gap-6"
             >
-              <button className="mt-10 text-[20px] font-semibold tracking-wide h-16 w-60 rounded-full text-white bg-orange-500 hover:scale-110">
-                Read More...
-              </button>
-
-              <button className="mt-10 text-[20px] font-semibold tracking-wide h-16 w-60 rounded-full text-orange-400 bg-gray-200 hover:scale-110 ease-in-out">
-                View Acheivements
-              </button>
+    
             </div>
           </div>
           <div className="w-2/5 float-right" id="left_halfpartofscreen">
             <img
-              className="mt-16 w-screen h-[500px] rounded-full border-8 border-orange-500"
+              className="mt-16 w-screen h-[500px] rounded-full border-8 border-green-500"
               src="..\public\images\photo-1488521787991-ed7bbaae773c.avif"
               alt=""
             />
